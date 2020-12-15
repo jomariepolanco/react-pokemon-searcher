a4ae8e359e9f391adeb8b1837b2fbdf82b875cf9
jomariepolanco <jomariepolanco@gmail.com>
2020-12-15 14:25:36 -0500

    create form pessimistically rendering

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -16,12 +16,33 @@ class PokemonPage extends React.Component {
     .then(pokemons => this.setState({api: pokemons}))
   }
 
+  createNewPokemon = (newPokeObj) => {
+    const newObj = {
+      name: newPokeObj.name,
+      hp: newPokeObj.hp,
+      sprites: {
+        front: newPokeObj.frontUrl,
+        back: newPokeObj.backUrl
+      }
+    }
+    fetch('http://localhost:3000/pokemon', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accepts": "application/json"
+      },
+      body: JSON.stringify(newObj)
+    })
+    .then(r => r.json())
+    .then(newPoke => this.setState({api: [...this.state.api, newPoke]}))
+  }
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm createNewPokemon={this.createNewPokemon}/>
         <br />
         <Search />
         <br />
