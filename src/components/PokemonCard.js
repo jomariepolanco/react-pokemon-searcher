3da3cd1703b04c2870be60910c163fd1671f7ce0
jomariepolanco <jomariepolanco@gmail.com>
2020-12-16 21:05:00 -0500

    done

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,12 +2,21 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  handleClick = (e) => {
+    // debugger
+    if (e.target.src === this.props.pokeObj.sprites.front){
+      e.target.src = this.props.pokeObj.sprites.back
+    } else {
+      e.target.src = this.props.pokeObj.sprites.front
+    }
+  }
+  
   render() {
     return (
       <Card>
         <div>
           <div className="image">
-            <img alt="oh no!" src={this.props.pokeObj.sprites.front}/>
+            <img alt="oh no!" onMouseOver={this.handleClick} src={this.props.pokeObj.sprites.front}/>
           </div>
           <div className="content">
             <div className="header">{this.props.pokeObj.name}</div>
